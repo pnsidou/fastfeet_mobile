b0993873dc4e9fbcc35a21567c76a99d6b2c594d
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-13 14:57:38 -0300

    feat: SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StatusBar, Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
-export interface Props {}
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/fastfeet-logo.png';
+
+import { Background, Container, Form, FormInput, SubmitButton } from './styles';
+
+interface Props {}
 
 const SignIn: React.FC<Props> = () => {
-  return <></>;
+  const dispatch = useDispatch();
+  const [deliverymanId, setDeliverymanId] = useState(null);
+
+  const loading = useSelector((state) => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(new Number(deliverymanId)));
+  }
+
+  return (
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Container>
+        <Image style={{ tintColor: 'white' }} source={logo} alt="" />
+        <Form>
+          <FormInput
+            keyboardType="numeric"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Type your deliveryman id..."
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={deliverymanId && deliverymanId?.toString()}
+            onChangeText={setDeliverymanId}
+          />
+
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Sign in
+          </SubmitButton>
+        </Form>
+      </Container>
+    </Background>
+  );
 };
 
 export default SignIn;
