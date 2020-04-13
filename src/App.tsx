741e906b9f455975fe33d218f00a43865c8d154a
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-13 14:52:30 -0300

    feat: signed and unsigned routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Routes from '~/routes';
 
 export default function App(): React.FC {
-  const isSigned = false;
+  const isSigned = useSelector((state) => state.auth.signed);
 
   return <Routes isSigned={isSigned} />;
 }
