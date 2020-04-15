3a39456a78504fe87dc7f040c843af5db9fbfe6a
Pedro N. Sidou <pnsidou@gmail.com>
2020-04-15 09:30:15 -0300

    feat: react-navigation screen header style

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 import Deliveries from '~/pages/Deliveries';
 import DeliveryInfo from '~/pages/DeliveryInfo';
@@ -13,7 +15,18 @@ export interface Props {}
 
 const Dashboard: React.FC<Props> = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: 'white',
+        headerTitleAlign: 'center',
+        headerLeft: ({ onPress }) => (
+          <BorderlessButton onPress={onPress} style={{ marginLeft: 25 }}>
+            <Icon name="chevron-left" size={25} color="white" />
+          </BorderlessButton>
+        ),
+      }}
+    >
       <Stack.Screen name="Deliveries" component={Deliveries} />
       <Stack.Screen name="DeliveryInfo" component={DeliveryInfo} />
       <Stack.Screen name="RegisterProblem" component={RegisterProblem} />
